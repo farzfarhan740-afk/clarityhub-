8b3557f866b4b11fef1794dd34fd5f73841ecbd4
farzfarhan740-afk <farzfarhan740@gmail.com>
2026-04-11 13:00:56 +0800

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
     headers: {
-      "Authorization": "Bearer " + process.env.OPENAI_API_KEY,
+      "Authorization": "Bearer " + process.env.sk_proj_UOptzZnSqOI11oMAqwFtZwHQ3P32k4V2Kb56SrFzaEtHNFZxZAPJTo7PxjYo3_oizc_EFbMF_XT3BlbkFJcTIgbd6XWeG5VNLgpd6MwpjPkhFkTNtNU5pSN6gzGOPtXmecam1u_80foxgj757h_Ya1_kLJkA,
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
